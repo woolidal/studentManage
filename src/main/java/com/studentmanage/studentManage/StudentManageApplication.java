d8f9f32fccfd7a9faba989260a0c080002f97aa9
woolidal <sidsid0120@naver.com>
2022-12-02 16:34:15 +0900

    Initial commit

---
+++ src/main/java/com/studentmanage/studentManage/StudentManageApplication.java
@@ -0,0 +1,13 @@
+package com.studentmanage.studentManage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManageApplication.class, args);
+	}
+
+}
