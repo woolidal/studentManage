e2589da627e973e2dbda554e80bfac7c30f6ba15
SooKyeom <sgkim169@gmail.com>
2022-12-08 11:27:07 +0900

    1208_메인공지_0.1

--- src/main/java/com/studentmanage/studentManage/StudentManageApplication.java
+++ src/main/java/com/studentmanage/studentManage/StudentManageApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentManageApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StudentManageApplication.class, args);
 	}
 
