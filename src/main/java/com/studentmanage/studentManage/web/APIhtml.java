abf765c1e503c2598750697e2b47e42a31dd9160
Kyeom <118805402+SooKyeom@users.noreply.github.com>
2022-12-09 17:16:19 +0900

    1209_자리배치추가_sk

--- src/main/java/com/studentmanage/studentManage/web/APIhtml.java
+++ src/main/java/com/studentmanage/studentManage/web/APIhtml.java
@@ -16,4 +16,10 @@ public class APIhtml {
         return "index";
     }
 
+    @RequestMapping(value = "class_seat", method = RequestMethod.GET)
+    public String clsseat() { return "class_seat"; }
+
+    @RequestMapping(value = "class_seat_edit", method = RequestMethod.GET)
+    public String clsseatedit() { return "class_seat_edit"; }
+
 }
--- src/main/java/com/studentmanage/studentManage/web/WebController.java
+++ src/main/java/com/studentmanage/studentManage/web/WebController.java
@@ -17,4 +17,14 @@ public class WebController {
 
         return "index";
     }
+
+    @RequestMapping("/class_seat")
+    public String clsseat(){
+        return "class_seat";
+    }
+
+    @RequestMapping("/class_seat_edit")
+    public String clsseatedit(){
+        return "class_seat_edit";
+    }
 }
