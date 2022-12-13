7252584cde7c1519c79962530a3659ba0ea178c2
Myoungchae <dcl7419@naver.com>
2022-12-13 19:53:30 +0900

    1213_학생관리_학급관리_연동

--- src/main/resources/static/js/demo/datatables-demo2.js
+++ src/main/resources/static/js/demo/datatables-demo2.js
@@ -1,8 +1,12 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-    var oTable = $('#dataTable1').dataTable({dom: 'lrt',info: false,paging:false, columnDefs: [{ targets: [0,1,2], visible:false }]});
+    var oTable = $('#dataTable1').dataTable({dom: 'lrt',info: false,paging:false,});
     $('select#subject').change( function () { oTable.fnFilter( this.value, 6); } );
     $('select#grade').change( function () { oTable.fnFilter( this.value, 0); } );
     $('select#semester').change( function () { oTable.fnFilter( this.value, 1); } );
     $('select#exam').change( function () { oTable.fnFilter( this.value, 2); } );
+
+    var oTable = $('#dataTable2').dataTable({dom: 'lrt',info: false,paging:false,});
 });
+
+//columnDefs: [{ targets: [0,1,2], visible:false }]
--- src/main/resources/static/js/demo/datatables-demo3.js
+++ src/main/resources/static/js/demo/datatables-demo3.js
@@ -1,7 +1,8 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-    var oTable = $('#dataTable1').dataTable({dom: 'lrt',info: false,paging:false, columnDefs: [{ targets: [0,1], visible:false }]});
+    var oTable = $('#dataTable1').dataTable({dom: 'lrt',info: false,displayLength: 4});
     $('select#subject').change( function () { oTable.fnFilter( this.value, 2); } );
     $('select#grade').change( function () { oTable.fnFilter( this.value, 0); } );
     $('select#semester').change( function () { oTable.fnFilter( this.value, 1); } );
-});``
+});
+// columnDefs: [{ targets: [0,1], visible:false }]
