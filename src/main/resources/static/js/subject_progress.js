cca17ad1976afc83950e4f0c55306f7fb067fc8d
Kyeom <118805402+SooKyeom@users.noreply.github.com>
2022-12-13 20:11:26 +0900

    1213_subject_seat_progress_check_kyeom

--- src/main/resources/static/js/subject_progress.js
+++ src/main/resources/static/js/subject_progress.js
@@ -3,7 +3,11 @@ function progCheck1() {
     const progCheck = document.getElementById("chk_1").checked;
     if (progCheck === true) {
         const date = new Date();
-        document.getElementById("pro1").innerText = date.toLocaleTimeString('ko-kr');
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDate()).slice(-2);
+        const dateStr = year + '/' + month + '/' + day;
+        document.getElementById("pro1").innerText = dateStr;
     } else {
         document.getElementById("pro1").innerText = "-";
     }
@@ -13,7 +17,11 @@ function progCheck2() {
     const progCheck = document.getElementById("chk_2").checked;
     if (progCheck === true) {
         const date = new Date();
-        document.getElementById("pro2").innerText = date.toLocaleTimeString('ko-kr');
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDate()).slice(-2);
+        const dateStr = year + '/' + month + '/' + day;
+        document.getElementById("pro2").innerText = dateStr;
     } else {
         document.getElementById("pro2").innerText = "-";
     }
@@ -23,7 +31,11 @@ function progCheck3() {
     const progCheck = document.getElementById("chk_3").checked;
     if (progCheck === true) {
         const date = new Date();
-        document.getElementById("pro3").innerText = date.toLocaleTimeString('ko-kr');
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDate()).slice(-2);
+        const dateStr = year + '/' + month + '/' + day;
+        document.getElementById("pro3").innerText = dateStr;
     } else {
         document.getElementById("pro3").innerText = "-";
     }
@@ -33,7 +45,11 @@ function progCheck4() {
     const progCheck = document.getElementById("chk_4").checked;
     if (progCheck === true) {
         const date = new Date();
-        document.getElementById("pro4").innerText = date.toLocaleTimeString('ko-kr');
+        const year = date.getFullYear();
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const day = ('0' + date.getDate()).slice(-2);
+        const dateStr = year + '/' + month + '/' + day;
+        document.getElementById("pro4").innerText = dateStr;
     } else {
         document.getElementById("pro4").innerText = "-";
     }
