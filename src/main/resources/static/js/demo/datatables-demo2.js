fc295f0f7edf4542e39b12a4b3e3626f9a021c1b
Myoungchae <dcl7419@naver.com>
2022-12-14 15:35:57 +0900

    1214_학생관리_학급관리_연동

--- src/main/resources/static/js/demo/datatables-demo2.js
+++ src/main/resources/static/js/demo/datatables-demo2.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
     $('select#exam').change( function () { oTable.fnFilter( this.value, 2); } );
 
     var oTable = $('#dataTable2').dataTable({dom: 'lrt',info: false,paging:false,});
+    var oTable = $('#dataTable3').dataTable({dom: 'lrt',info: false,paging:false,});
+    var oTable = $('#dataTable4').dataTable({dom: 'lrt',info: false,paging:false,});
+    var oTable = $('#dataTable5').dataTable({dom: 'lrt',info: false,paging:false,});
+    var oTable = $('#dataTable6').dataTable({dom: 'lrt',info: false,paging:false,});
 });
 
 //columnDefs: [{ targets: [0,1,2], visible:false }]
